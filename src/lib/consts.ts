d65a52870e127a8fe2a1f3ce5725196a35281738
Twijn <twijn@twijn.net>
2025-11-04 09:03:10 -0600

    fix: update APP_NAME to use PUBLIC_APP_NAME for better environment configuration

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,4 +1,6 @@
+import { PUBLIC_APP_NAME } from '$env/static/public';
+
 declare const __APP_VERSION__: string;
 
-export const APP_NAME = 'Svelte Boilerplate';
+export const APP_NAME = PUBLIC_APP_NAME ?? 'Svelte Boilerplate';
 export const APP_VERSION = __APP_VERSION__;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,10 @@
-import { redirect } from "@sveltejs/kit"
-import type { PageServerLoad } from "./$types"
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-    if (!locals?.user?.id) {
-        throw redirect(302, "/login");
-    } else {
-        throw redirect(302, "/panel");
-    }
-}
+	if (!locals?.user?.id) {
+		throw redirect(302, '/login');
+	} else {
+		throw redirect(302, '/panel');
+	}
+};
